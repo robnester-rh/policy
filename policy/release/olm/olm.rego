enterprise-contract/ec-policies

Add pipeline_intention field to rule metadata
Extend Rego policy rule metadata to include a pipeline_intention field
that declares when rules are relevant: "release", "staging", or null (always).

Changes:
- Add pipeline_intention field to custom metadata section in YAML array format
- Update schedule rules (weekday_restriction, date_restriction) with [release, production]
- Update quay_expiration rule (expires_label) with [release, production, staging]
- Update OLM rules (unpinned_snapshot_references, inaccessible_related_images,
  unmapped_references) with [release, production, staging]

The field is optional and defaults to null (non-release-bound) for backwards
compatibility. Existing helper functions (_release_restrictions_apply,
_schedule_restrictions_apply) remain unchanged to maintain current behavior.

Pipeline intention values align with existing runtime checks in rule data.

https://issues.redhat.com/browse/EC-1338

policy/release/olm/olm.rego
@@ -132,6 +132,10 @@ deny contains result if {
 #   the image being pulled.
 # custom:
 #   short_name: unpinned_snapshot_references
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The %q image reference is not pinned in the input snapshot.
 #   solution: >-
 #     Update the input snapshot replacing the unpinned image reference with pinned image
@@ -141,7 +145,7 @@ deny contains result if {
 #   effective_on: 2024-08-15T00:00:00Z
 #
 deny contains result if {
-	_release_restrictions_apply
+	lib.release_restrictions_apply(rego.metadata.chain())
 
 	input_image = image.parse(input.image.ref)
 	components := input.snapshot.components
@@ -160,6 +164,10 @@ deny contains result if {
 #   Ensure all related image references include a digest.
 # custom:
 #   short_name: unpinned_related_images
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: "%d related images are not pinned with a digest: %s."
 #   solution: >-
 #     Update the related images replacing the unpinned image reference
@@ -168,7 +176,7 @@ deny contains result if {
 #   - redhat
 #
 deny contains result if {
-	_release_restrictions_apply
+	lib.release_restrictions_apply(rego.metadata.chain())
 
 	unpinned_related_images := [related |
 		some related in _related_images_not_in_snapshot
@@ -192,6 +200,10 @@ deny contains result if {
 #   Ensure that all images are accessible.
 # custom:
 #   short_name: inaccessible_related_images
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The %q related image reference is not accessible.
 #   solution: >-
 #     Ensure all related images are available. The related images are defined by
@@ -202,7 +214,7 @@ deny contains result if {
 #   effective_on: 2025-03-10T00:00:00Z
 #
 deny contains result if {
-	_release_restrictions_apply
+	lib.release_restrictions_apply(rego.metadata.chain())
 
 	some unmatched_image in _related_images_not_in_snapshot
 	unmatched_ref := _image_ref(unmatched_image)
@@ -255,6 +267,10 @@ deny contains result if {
 #   already.
 # custom:
 #   short_name: unmapped_references
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The %q CSV image reference is not in the snapshot or accessible.
 #   solution: >-
 #     Add the missing image to the snapshot or check if the CSV pullspec
@@ -263,7 +279,7 @@ deny contains result if {
 #   - redhat
 #   effective_on: 2024-08-15T00:00:00Z
 deny contains result if {
-	_release_restrictions_apply
+	lib.release_restrictions_apply(rego.metadata.chain())
 
 	snapshot_components := input.snapshot.components
 	component_images_digests := [component_image.digest |
@@ -567,13 +583,6 @@ _subscriptions_errors contains error if {
 }
 
 _subscription_annotation := "operators.openshift.io/valid-subscription"
-
-# We want these checks to apply only if we're doing a release.
-default _release_restrictions_apply := false
-
-_release_restrictions_apply if {
-	lib.rule_data("pipeline_intention") in {"release", "production", "staging"}
-}
 
 # Used by allowed_registries
 _image_registry_allowed(image_repo, allowed_prefixes) if {

policy/release/olm/olm_test.rego
@@ -421,7 +421,8 @@ mock_ec_oci_image_descriptor("registry.io/repo/msd:latest") := `{"config": {"dig
 
 test_olm_ci_pipeline if {
 	# Make sure no violations are thrown if it isn't a release pipeline
-	lib.assert_equal(false, olm._release_restrictions_apply) with data.rule_data as {"pipeline_intention": null}
+	# regal ignore:line-length
+	lib.assert_equal(false, lib.release_restrictions_apply(rego.metadata.chain())) with data.rule_data as {"pipeline_intention": null}
 }
 
 test_mock_cafe_descriptor if {

policy/release/quay_expiration/quay_expiration.rego
@@ -22,6 +22,10 @@ import data.lib
 #   the value of the `pipeline_intention` rule data.
 # custom:
 #   short_name: expires_label
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The image has a 'quay.expires-after' label set to '%s'
 #   solution: >-
 #     Make sure the image is built without setting the "quay.expires-after" label. This
@@ -31,7 +35,7 @@ import data.lib
 #   - redhat
 #
 deny contains result if {
-	_expires_label_check_applies
+	lib.release_restrictions_apply(rego.metadata.chain())
 
 	# This is where we can access the image labels
 	some label_name, label_value in input.image.config.Labels
@@ -46,11 +50,4 @@ deny contains result if {
 
 	# Send up the violation the details
 	result := lib.result_helper(rego.metadata.chain(), [label_value])
-}
-
-# The check only applies if we're intending to release the image
-default _expires_label_check_applies := false
-
-_expires_label_check_applies if {
-	lib.rule_data("pipeline_intention") in {"release", "production", "staging"}
 }

policy/release/schedule/schedule.rego
@@ -20,6 +20,9 @@ import data.lib.json as j
 #   pipeline, as determined by the value of the `pipeline_intention` rule data.
 # custom:
 #   short_name: weekday_restriction
+#   pipeline_intention:
+#   - release
+#   - production
 #   failure_msg: '%s is a disallowed weekday: %s'
 #   solution: Try again on a different weekday.
 #   collections:
@@ -27,7 +30,7 @@ import data.lib.json as j
 #   - redhat_rpms
 #
 deny contains result if {
-	_schedule_restrictions_apply
+	lib.release_restrictions_apply(rego.metadata.chain())
 	today := lower(time.weekday(lib.time.effective_current_time_ns))
 	disallowed := {lower(w) | some w in lib.rule_data("disallowed_weekdays")}
 	count(disallowed) > 0
@@ -45,6 +48,9 @@ deny contains result if {
 #   `pipeline_intention` rule data.
 # custom:
 #   short_name: date_restriction
+#   pipeline_intention:
+#   - release
+#   - production
 #   failure_msg: '%s is a disallowed date: %s'
 #   solution: Try again on a different day.
 #   collections:
@@ -52,7 +58,7 @@ deny contains result if {
 #   - redhat_rpms
 #
 deny contains result if {
-	_schedule_restrictions_apply
+	lib.release_restrictions_apply(rego.metadata.chain())
 	today := time.format([lib.time.effective_current_time_ns, "UTC", "2006-01-02"])
 	disallowed := lib.rule_data("disallowed_dates")
 	today in disallowed
@@ -77,15 +83,6 @@ deny contains result if {
 	# (For this one let's do it always)
 	some e in _rule_data_errors
 	result := lib.result_helper_with_severity(rego.metadata.chain(), [e.message], e.severity)
-}
-
-# We want these checks to apply only if we're doing a release. Detect that by checking
-# the `pipeline_intention` value which is set to "release" or "production" for Konflux release pipelines.
-# Notably, the value "staging" is not checked here. The disallowed dates rule doesn't apply to staging releases.
-default _schedule_restrictions_apply := false
-
-_schedule_restrictions_apply if {
-	lib.rule_data("pipeline_intention") in {"release", "production"} # But not staging
 }
 
 _rule_data_errors contains error if {

robnester-rh/policy

policy/release/olm/olm.rego
@@ -132,6 +132,10 @@ deny contains result if {
 #   the image being pulled.
 # custom:
 #   short_name: unpinned_snapshot_references
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The %q image reference is not pinned in the input snapshot.
 #   solution: >-
 #     Update the input snapshot replacing the unpinned image reference with pinned image
@@ -141,7 +145,7 @@ deny contains result if {
 #   effective_on: 2024-08-15T00:00:00Z
 #
 deny contains result if {
-	_release_restrictions_apply
+	lib.pipeline_intention_match(rego.metadata.chain())
 
 	input_image = image.parse(input.image.ref)
 	components := input.snapshot.components
@@ -160,6 +164,10 @@ deny contains result if {
 #   Ensure all related image references include a digest.
 # custom:
 #   short_name: unpinned_related_images
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: "%d related images are not pinned with a digest: %s."
 #   solution: >-
 #     Update the related images replacing the unpinned image reference
@@ -168,7 +176,7 @@ deny contains result if {
 #   - redhat
 #
 deny contains result if {
-	_release_restrictions_apply
+	lib.pipeline_intention_match(rego.metadata.chain())
 
 	unpinned_related_images := [related |
 		some related in _related_images_not_in_snapshot
@@ -192,6 +200,10 @@ deny contains result if {
 #   Ensure that all images are accessible.
 # custom:
 #   short_name: inaccessible_related_images
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The %q related image reference is not accessible.
 #   solution: >-
 #     Ensure all related images are available. The related images are defined by
@@ -202,7 +214,7 @@ deny contains result if {
 #   effective_on: 2025-03-10T00:00:00Z
 #
 deny contains result if {
-	_release_restrictions_apply
+	lib.pipeline_intention_match(rego.metadata.chain())
 
 	some unmatched_image in _related_images_not_in_snapshot
 	unmatched_ref := _image_ref(unmatched_image)
@@ -255,6 +267,10 @@ deny contains result if {
 #   already.
 # custom:
 #   short_name: unmapped_references
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The %q CSV image reference is not in the snapshot or accessible.
 #   solution: >-
 #     Add the missing image to the snapshot or check if the CSV pullspec
@@ -263,7 +279,7 @@ deny contains result if {
 #   - redhat
 #   effective_on: 2024-08-15T00:00:00Z
 deny contains result if {
-	_release_restrictions_apply
+	lib.pipeline_intention_match(rego.metadata.chain())
 
 	snapshot_components := input.snapshot.components
 	component_images_digests := [component_image.digest |
@@ -567,13 +583,6 @@ _subscriptions_errors contains error if {
 }
 
 _subscription_annotation := "operators.openshift.io/valid-subscription"
-
-# We want these checks to apply only if we're doing a release.
-default _release_restrictions_apply := false
-
-_release_restrictions_apply if {
-	lib.rule_data("pipeline_intention") in {"release", "production", "staging"}
-}
 
 # Used by allowed_registries
 _image_registry_allowed(image_repo, allowed_prefixes) if {

policy/release/olm/olm_test.rego
@@ -421,7 +421,8 @@ mock_ec_oci_image_descriptor("registry.io/repo/msd:latest") := `{"config": {"dig
 
 test_olm_ci_pipeline if {
 	# Make sure no violations are thrown if it isn't a release pipeline
-	lib.assert_equal(false, olm._release_restrictions_apply) with data.rule_data as {"pipeline_intention": null}
+	# regal ignore:line-length
+	lib.assert_equal(false, lib.pipeline_intention_match(rego.metadata.chain())) with data.rule_data as {"pipeline_intention": null}
 }
 
 test_mock_cafe_descriptor if {

policy/release/quay_expiration/quay_expiration.rego
@@ -22,6 +22,10 @@ import data.lib
 #   the value of the `pipeline_intention` rule data.
 # custom:
 #   short_name: expires_label
+#   pipeline_intention:
+#   - release
+#   - production
+#   - staging
 #   failure_msg: The image has a 'quay.expires-after' label set to '%s'
 #   solution: >-
 #     Make sure the image is built without setting the "quay.expires-after" label. This
@@ -31,7 +35,7 @@ import data.lib
 #   - redhat
 #
 deny contains result if {
-	_expires_label_check_applies
+	lib.pipeline_intention_match(rego.metadata.chain())
 
 	# This is where we can access the image labels
 	some label_name, label_value in input.image.config.Labels
@@ -46,11 +50,4 @@ deny contains result if {
 
 	# Send up the violation the details
 	result := lib.result_helper(rego.metadata.chain(), [label_value])
-}
-
-# The check only applies if we're intending to release the image
-default _expires_label_check_applies := false
-
-_expires_label_check_applies if {
-	lib.rule_data("pipeline_intention") in {"release", "production", "staging"}
 }

policy/release/schedule/schedule.rego
@@ -20,6 +20,9 @@ import data.lib.json as j
 #   pipeline, as determined by the value of the `pipeline_intention` rule data.
 # custom:
 #   short_name: weekday_restriction
+#   pipeline_intention:
+#   - release
+#   - production
 #   failure_msg: '%s is a disallowed weekday: %s'
 #   solution: Try again on a different weekday.
 #   collections:
@@ -27,7 +30,7 @@ import data.lib.json as j
 #   - redhat_rpms
 #
 deny contains result if {
-	_schedule_restrictions_apply
+	lib.pipeline_intention_match(rego.metadata.chain())
 	today := lower(time.weekday(lib.time.effective_current_time_ns))
 	disallowed := {lower(w) | some w in lib.rule_data("disallowed_weekdays")}
 	count(disallowed) > 0
@@ -45,6 +48,9 @@ deny contains result if {
 #   `pipeline_intention` rule data.
 # custom:
 #   short_name: date_restriction
+#   pipeline_intention:
+#   - release
+#   - production
 #   failure_msg: '%s is a disallowed date: %s'
 #   solution: Try again on a different day.
 #   collections:
@@ -52,7 +58,7 @@ deny contains result if {
 #   - redhat_rpms
 #
 deny contains result if {
-	_schedule_restrictions_apply
+	lib.pipeline_intention_match(rego.metadata.chain())
 	today := time.format([lib.time.effective_current_time_ns, "UTC", "2006-01-02"])
 	disallowed := lib.rule_data("disallowed_dates")
 	today in disallowed
@@ -77,15 +83,6 @@ deny contains result if {
 	# (For this one let's do it always)
 	some e in _rule_data_errors
 	result := lib.result_helper_with_severity(rego.metadata.chain(), [e.message], e.severity)
-}
-
-# We want these checks to apply only if we're doing a release. Detect that by checking
-# the `pipeline_intention` value which is set to "release" or "production" for Konflux release pipelines.
-# Notably, the value "staging" is not checked here. The disallowed dates rule doesn't apply to staging releases.
-default _schedule_restrictions_apply := false
-
-_schedule_restrictions_apply if {
-	lib.rule_data("pipeline_intention") in {"release", "production"} # But not staging
 }
 
 _rule_data_errors contains error if {